629a0cf161e26b6f23bd532661854da18fe3231d
vskoshkin <Jumpykoshkin@gmail.com>
2024-03-24 01:01:30 +0300

    upgrade video attach

--- src/test/java/tests/RegistrationPageWithTestData.java
+++ src/test/java/tests/RegistrationPageWithTestData.java
@@ -8,12 +8,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import testdata.testData;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
@@ -31,28 +30,12 @@ public class RegistrationPageWithTestData extends TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        ChromeOptions options = new ChromeOptions();
-
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
-            put("name", "Test badge...");
-
-            /* How to set session timeout */
-            put("sessionTimeout", "15m");
-
-            /* How to set timezone */
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
-
-            /* How to add "trash" button */
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-
-            /* How to enable video recording */
-            put("enableVideo", true);
-        }});
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
